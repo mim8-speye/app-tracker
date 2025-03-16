b0911b89ac245434781a8704fabe4bf097fc39d9
mim8-speye <speye@duck.com>
2025-03-16 23:52:59 +0100

    Populate assignee select

---
+++ app/api/users/route.ts
@@ -0,0 +1,15 @@
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // Your logic here
+  const users = await prisma.user.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  if (!users) {
+    return NextResponse.json({ message: "No users found" }, { status: 404 });
+  }
+  return NextResponse.json(users, { status: 200 });
+}
--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,21 +1,31 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const IssueSelect = () => {
+  const [users, setUsers] = React.useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await axios.get<User[]>("/api/users");
+      setUsers(response.data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="orange">Naran</Select.Item>
-        </Select.Group>
-        <Select.Separator />
-        <Select.Group>
-          <Select.Label>Vegetables</Select.Label>
-          <Select.Item value="carrot">Carrot</Select.Item>
-          <Select.Item value="potato">Potato</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
