22abc5a6cf73d5229f8439ad353d31b533a6f0a9
mim8-speye <speye@duck.com>
2025-03-16 22:21:06 +0100

    Refactor: NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -16,8 +16,63 @@ import {
 } from "@radix-ui/themes";
 
 const NavBar = () => {
-  const pathname = usePathname();
+  return (
+    <nav className="mb-5 py-4 border-b-2 border-gray-200 p-5">
+      <Container>
+        <Flex justify={"between"}>
+          <Flex align={"center"} gap={"3"}>
+            <Link href={"/"}>
+              <LuMessageCircleWarning className="size-5"></LuMessageCircleWarning>
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+const AuthStatus = () => {
   const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href={"/api/auth/signin"}>
+        Log in
+      </Link>
+    );
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            size={"2"}
+            radius="full"
+            src={session!.user!.image!}
+            fallback="?"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size={"2"}>{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href={"/api/auth/signout"}>Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const NavLinks = () => {
+  const pathname = usePathname();
 
   const links = [
     {
@@ -31,64 +86,21 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="mb-5 py-4 border-b-2 border-gray-200 p-5">
-      <Container>
-        <Flex justify={"between"}>
-          <Flex align={"center"} gap={"3"}>
-            <div>
-              <Link href={"/"}>
-                <LuMessageCircleWarning className="size-5"></LuMessageCircleWarning>
-              </Link>
-            </div>
-            <ul className="flex space-x-5 items-center">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    href={link.href}
-                    className={classNames({
-                      "text-gray-800": pathname === link.href,
-                      "text-gray-500": pathname !== link.href,
-                      "hover:text-gray-800 transition-colors duration-200":
-                        true,
-                    })}
-                  >
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    size={"2"}
-                    radius="full"
-                    src={session.user!.image!}
-                    fallback="?"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size={"2"}>{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href={"/api/auth/signout"}>Log out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-              //<Link href={"/api/auth/signout"}>Log out</Link>
-            )}
-            {status === "unauthenticated" && (
-              <Link href={"/api/auth/signin"}>Log in</Link>
-            )}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+    <ul className="flex space-x-5 items-center">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={classNames({
+              "nav-link": true,
+              "!text-gray-800": pathname === link.href,
+            })}
+          >
+            {link.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
