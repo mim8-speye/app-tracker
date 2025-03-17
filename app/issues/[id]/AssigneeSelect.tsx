b061aa781154ec55b44a4f18a4cdda0bcc700437
mim8-speye <speye@duck.com>
2025-03-17 01:09:53 +0100

    Refactor: AssigneeSelect

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -7,16 +7,7 @@ import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 
 const IssueSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000,
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton height={"2rem"} />;
 
@@ -55,5 +46,13 @@ const IssueSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
 export default IssueSelect;
