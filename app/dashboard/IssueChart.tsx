77c04adc2c105ea0a72a6679b905ffcf0cb7f365
mim8-speye <speye@duck.com>
2025-03-19 16:21:04 +0100

    Made Bar look colored

--- app/dashboard/IssueChart.tsx
+++ app/dashboard/IssueChart.tsx
@@ -2,7 +2,14 @@
 
 import { Card } from "@radix-ui/themes";
 import React from "react";
-import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar } from "recharts";
+import {
+  ResponsiveContainer,
+  BarChart,
+  XAxis,
+  YAxis,
+  Bar,
+  Cell,
+} from "recharts";
 
 interface Props {
   open: number;
@@ -11,23 +18,22 @@ interface Props {
 }
 
 const IssueChart = ({ open, closed, inProgress }: Props) => {
+  const data = [
+    { name: "Open", value: open, color: "var(--accent-9)" },
+    { name: "In Progress", value: inProgress, color: "orange" },
+    { name: "Closed", value: closed, color: "red" },
+  ];
   return (
     <Card>
       <ResponsiveContainer width={"100%"} height={300}>
-        <BarChart
-          data={[
-            { name: "Open", value: open },
-            { name: "In Progress", value: inProgress },
-            { name: "Closed", value: closed },
-          ]}
-        >
+        <BarChart data={data}>
           <XAxis dataKey="name" />
           <YAxis />
-          <Bar
-            barSize={60}
-            dataKey="value"
-            style={{ fill: "var(--accent-9)" }}
-          />
+          <Bar dataKey="value" barSize={60}>
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color} />
+            ))}
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     </Card>
